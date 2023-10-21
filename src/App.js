5e62758667dd63ea66c37842375e0168d6ba891d
Nyijia Morgan <mnyijia23@gmail.com>
2023-10-21 10:26:43 -0600

    deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import { Container, Row, Col, Button, Card, Form } from 'react-bootstrap'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -13,7 +13,7 @@ function Index(props) {
           <ul class="menu align-center">
             <li><a href="https://github.com/nmorgan24">Github</a></li>
             <li><a href="https://get.foundation/index.html">Foundation</a></li>
-            <li><a href="https://generalassemb.ly/?topic=&mkt_account_id=1056949875&mkt_campaign_id=968359094&mkt_ad_group_id=50685436960&mkt_device_type=c&mkt_keyword=general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=663321068398&mkt_network=g&mkt_target_id=kwd-300765785657&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:DAL:BR:GeneralAssembly&utm_content=campus-lead-lander&utm_term=general%20assembly&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D5-p0ngNvKh4ZxNwlfCYD8IKEZqG3J3gPkqZmUnHotdGV9wrpM_CEf8aAkQtEALw_wcB&gclsrc=aw.ds"></a>General Assembly</li>
+            <li><a href="https://generalassemb.ly/?topic=&mkt_account_id=1056949875&mkt_campaign_id=968359094&mkt_ad_group_id=50685436960&mkt_device_type=c&mkt_keyword=general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=663321068398&mkt_network=g&mkt_target_id=kwd-300765785657&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:DAL:BR:GeneralAssembly&utm_content=campus-lead-lander&utm_term=general%20assembly&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D5-p0ngNvKh4ZxNwlfCYD8IKEZqG3J3gPkqZmUnHotdGV9wrpM_CEf8aAkQtEALw_wcB&gclsrc=aw.ds">General Assembly</a></li>
           </ul>
         </div>
         <div class="columns sticky-footer-css-body text-center">
@@ -39,7 +39,7 @@ function Index(props) {
           <ul class="menu align-center">
           <li><a href="https://github.com/nmorgan24">Github</a></li>
             <li><a href="https://get.foundation/index.html">Foundation</a></li>
-            <li><a href="https://generalassemb.ly/?topic=&mkt_account_id=1056949875&mkt_campaign_id=968359094&mkt_ad_group_id=50685436960&mkt_device_type=c&mkt_keyword=general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=663321068398&mkt_network=g&mkt_target_id=kwd-300765785657&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:DAL:BR:GeneralAssembly&utm_content=campus-lead-lander&utm_term=general%20assembly&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D5-p0ngNvKh4ZxNwlfCYD8IKEZqG3J3gPkqZmUnHotdGV9wrpM_CEf8aAkQtEALw_wcB&gclsrc=aw.ds"></a>General Assembly</li>
+            <li><a href="https://generalassemb.ly/?topic=&mkt_account_id=1056949875&mkt_campaign_id=968359094&mkt_ad_group_id=50685436960&mkt_device_type=c&mkt_keyword=general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=663321068398&mkt_network=g&mkt_target_id=kwd-300765785657&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:DAL:BR:GeneralAssembly&utm_content=campus-lead-lander&utm_term=general%20assembly&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D5-p0ngNvKh4ZxNwlfCYD8IKEZqG3J3gPkqZmUnHotdGV9wrpM_CEf8aAkQtEALw_wcB&gclsrc=aw.ds">General Assembly</a></li>
           </ul>
         </div>
       </div>
